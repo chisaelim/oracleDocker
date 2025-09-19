9e1a65b3a113e741112995d4c3be1e00922ddb5d
chisae0123 <chisae0123@gmail.com>
2025-09-19 13:31:18 +0700

    web

--- web-app/includes/header.php
+++ web-app/includes/header.php
@@ -45,7 +45,7 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
+                <ul class="navbar-nav mx-auto">
                     <li class="nav-item">
                         <a class="nav-link <?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>"
                             href="index.php">
